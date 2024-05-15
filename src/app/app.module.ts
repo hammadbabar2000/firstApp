6fd21a268e5f8ff3e9137832eacadc7f50795a25
Hammad Babar <hammadbabar2000@outlook.com>
2024-05-15 17:11:43 -0400

    Inital

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,31 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { MatTabsModule } from '@angular/material/tabs' ;
+import { MatIconModule } from '@angular/material/icon';
+import { MainSheridanComponent } from './tabcomponents/main-sheridan/main-sheridan.component';
+import { MainCountryComponent } from './tabcomponents/main-country/main-country.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    MainSheridanComponent,
+    MainCountryComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatTabsModule,
+    MatIconModule
   ],
   providers: [
-    provideClientHydration()
+    provideClientHydration(),
+    provideAnimationsAsync()
   ],
   bootstrap: [AppComponent]
 })
